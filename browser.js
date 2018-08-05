cee3589ec102b7da53cfa912f277c2cf72b403eb
Justin Sisley <jsisley@paypal.com>
2018-08-05 16:00:27 -0700

    refactor: Refactored; added tests; cleaned up dependencies

--- browser.js
+++ browser.js
@@ -1,20 +1,17 @@
-'use strict';
+"use strict";
 
 var config = {
   // Key used to save the JWT to the browser's localStorage
   localStorageKey: 'token'
 };
-
 var tokenpress = {
   browser: {
     configure: function configure(_ref) {
       var localStorageKey = _ref.localStorageKey;
-
       config = Object.assign({}, config, {
         localStorageKey: localStorageKey
       });
     },
-
     save: function save(token) {
       window.localStorage.setItem(config.localStorageKey, token);
     },
@@ -25,45 +22,49 @@ var tokenpress = {
       window.localStorage.removeItem(config.localStorageKey);
     },
     isExpired: function isExpired() {
-      var token = window.localStorage.getItem(config.localStorageKey);
+      var token = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : window.localStorage.getItem(config.localStorageKey);
+
       if (!token) {
         return true;
       }
 
       var split = token.split('.');
+
       if (split.length < 3) {
         return true;
       }
 
-      var payload = void 0;
+      var payload;
+
       try {
-        payload = JSON.parse(atob(split[1]));
-      } catch (error) {/* no-op */}
+        payload = JSON.parse(window.atob(split[1]));
+      } catch (error) {
+        /* no-op */
+      }
+
       if (!payload || !payload.exp) {
         return true;
       }
 
       var expiration = payload.exp;
-
-      var expTimestamp = +('' + expiration).padEnd(13, '0');
+      var expTimestamp = +"".concat(expiration).padEnd(13, '0');
       var nowTimestamp = Date.now();
-
       return nowTimestamp > expTimestamp;
     },
     decode: function decode(token) {
       if (!token || !token.split) return null;
-
       var split = token.split('.');
       if (split.length < 3) return null;
-
       var payload = null;
+
       try {
-        payload = JSON.parse(atob(split[1]));
-      } catch (error) {/* no-op */}
+        payload = JSON.parse(window.atob(split[1]));
+      } catch (error) {
+        /* no-op */
+      }
 
       return payload;
     }
   }
 };
-
 module.exports = tokenpress;
---
+++ test.js
@@ -0,0 +1,76 @@
+const browser = require('./lib/browser');
+const node = require('./lib/node');
+
+describe('browser', () => {
+  it('should export an object', () => {
+    expect(typeof browser).toBe('object');
+  });
+
+  it('should allow the localStorage key to be customized', () => {
+    const sampleToken = '12345';
+
+    // Save to the default key
+    browser.save(sampleToken);
+
+    // Change the key
+    browser.configure({
+      localStorageKey: 'test-key',
+    });
+
+    // No token exists for the new key
+    expect(browser.get()).toBe(null);
+
+    // Save under the new key and verify
+    browser.save(sampleToken);
+    expect(browser.get()).toBe(sampleToken);
+
+    // Cleanup
+    browser.delete();
+  });
+
+  it('should save, get, and delete tokens', () => {
+    const sampleToken = '12345';
+
+    // Should not exist yet
+    expect(browser.get()).toBe(null);
+
+    // Save it and verify it's there
+    browser.save(sampleToken);
+    expect(browser.get()).toBe(sampleToken);
+
+    // Delete it and verify it's gone
+    browser.delete();
+    expect(browser.get()).toBe(null);
+  });
+
+  it('should verify if a token has expired', async (done) => {
+    // Configure with a 1-second expiration
+    node.configure({
+      secret: 'test',
+      expiresIn: 1,
+    });
+
+    // Create a new token
+    const token = await node.jwt.sign({
+      username: 'test_username',
+    });
+
+    // Save it to localStorage
+    browser.save(token);
+
+    // Make sure it hasn't expired yet
+    expect(browser.isExpired()).toBe(false);
+    expect(browser.isExpired(token)).toBe(false);
+
+    setTimeout(() => {
+      // It should now be expired
+      expect(browser.isExpired()).toBe(true);
+      expect(browser.isExpired(token)).toBe(true);
+
+      // Cleanup
+      browser.delete();
+
+      done();
+    }, 1000);
+  });
+});
